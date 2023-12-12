4f6c06ddb2454d9adab9e540504aa2035e432d4d
Shravan <shravansinghchaudhary@gmail.com>
2023-12-12 18:17:45 +0530

    chore: setup Prettier

--- apps/server/src/server.ts
+++ apps/server/src/server.ts
@@ -1,5 +1,5 @@
 const login = (name: string, password: string) => {
-  console.log(`login with ${name} and ${password}`)
-}
+  console.log(`login with ${name} and ${password}`);
+};
 
-login('shravan', 'ahsdkfh')
+login("shravan", "ahsdkfh");
